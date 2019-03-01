45ca19f816ca9b5ea5f93693081b4d6e8d58411b
Ispire <ispirett@gmail.com>
2019-02-28 20:48:29 -1000

    added switch case for position parameter

--- Utilties.js
+++ Utilties.js
@@ -1,15 +1,23 @@
 import {Platform, ToastAndroid} from "react-native";
 
-
-
-
  const TOAST =  (msg, duration, position=ToastAndroid.CENTER) => {
-
     //Check if duration = short or long
-
         try {
             const durationCheck = duration === "short" ? ToastAndroid.SHORT:ToastAndroid.LONG;
+            //Check and choose which option user chooses else return center.
+            switch(position){
 
+                case "top":
+                    position = ToastAndroid.TOP;
+                    break;
+                case "bottom":
+                    position = ToastAndroid.BOTTOM;
+                    break;
+                default:
+                    position = ToastAndroid.CENTER
+
+            }
+            // Check platform if android show toast else if ios show alert
             if (Platform.OS !== "ios") {
                 ToastAndroid.showWithGravity(msg, durationCheck, position );
             }
@@ -20,7 +28,6 @@ import {Platform, ToastAndroid} from "react-native";
         } catch (error) {
             console.log("TOAST ERROR: msg must be string  ")
         }
-
 };
 
 
